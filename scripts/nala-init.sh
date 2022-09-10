ce065b175675f2bf3566f90d61dd2a3d19a09e6c
Madalin Popa <github@madalintech.com>
2022-09-10 23:56:17 +0300

    add nala-init.sh

---
+++ scripts/nala-init.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+#  ███    ███ ██████
+#  ████  ████ ██   ██
+#  ██ ████ ██ ██████
+#  ██  ██  ██ ██
+#  ██      ██ ██
+#
+#
+# Author: Madalin Popa
+# Version: 0.1.0
+# Email: github@madalinpopa.com
+
+# Ubuntu
+sudo apt update && sudo apt install nala
