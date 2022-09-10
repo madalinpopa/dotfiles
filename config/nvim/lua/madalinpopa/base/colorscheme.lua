7b851c34ed5e4f8cd81e9efc10d62ac2e9be9d1e
Madalin Popa <github@madalintech.com>
2022-09-10 20:01:25 +0300

    add nvim config

---
+++ config/nvim/lua/madalinpopa/base/colorscheme.lua
@@ -0,0 +1,12 @@
+------------------------------------------------------
+-- Author: Madalin Popa
+-- Email: contact@madalinpopa.com
+------------------------------------------------------
+
+local colorscheme = "tokyonight"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
