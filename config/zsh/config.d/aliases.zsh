0a1233596c98ba95dda7d57a6f13e56581e7995a
Madalin Popa <contact@madalinpopa.com>
2022-10-05 22:20:01 +0300

    chore: Update zsh configuration

---
+++ config/zsh/config.d/aliases.zsh
@@ -0,0 +1,31 @@
+alias ll='ls -lah'
+alias la='ls -A'
+alias l='ls'
+
+# Aliases to protect against overwriting
+alias cp='cp -i'
+alias mv='mv -i'
+
+# git related aliases
+alias gag='git exec ag'
+
+
+# Update dotfiles
+dfu() {
+    (
+        cd ~/.dotfiles && git pull --ff-only && ./install -q
+    )
+}
+
+# nala alias
+apt() {
+    command nala "$@"
+}
+sudo() {
+    if [ "$1" = "apt" ]; then
+        shift
+        command sudo nala "$@"
+    else
+        command sudo "$@"
+    fi
+}
---
+++ config/zsh/config.d/exports.zsh
@@ -0,0 +1,17 @@
+
+# You may need to manually set your language environment
+export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='vim'
+else
+  export EDITOR='vim'
+fi
+
+# anki blank window
+export QTWEBENGINE_CHROMIUM_FLAGS="--no-sandbox"
+
+# Nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
