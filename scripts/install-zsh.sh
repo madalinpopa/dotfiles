86d308b005100d07e67158307047856c8c5ba680
Madalin Popa <contact@madalinpopa.com>
2022-10-05 20:33:07 +0300

    chore: Add zshrc setup

---
+++ scripts/install-zsh.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+#  ███    ███ ██████
+#  ████  ████ ██   ██
+#  ██ ████ ██ ██████
+#  ██  ██  ██ ██
+#  ██      ██ ██
+#
+#
+# Author: Madalin Popa
+# Version: 0.1.0
+# Email: github@madalinpopa.com
+
+apt-get install zsh
+
+chsh -s $(which zsh)
