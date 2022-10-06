aad772b3d9313ea3bae9bc10c984abbbc27e2e19
Madalin Popa <contact@madalinpopa.com>
2022-10-06 10:14:58 +0000

    chore: Update nvim-tree config

--- config/nvim/lua/madalinpopa/config/nvim-tree.lua
+++ config/nvim/lua/madalinpopa/config/nvim-tree.lua
@@ -27,96 +27,98 @@
 -- SETUP
 ------------------------------------------------------
 
-  nvim_tree.setup {
-    disable_netrw = true,
-    hijack_netrw = true,
-    open_on_setup = false,
-    ignore_ft_on_setup = {
-      "startify",
-      "dashboard",
-      "alpha",
-    },
-    -- auto_close = true,
-    open_on_tab = false,
-    hijack_cursor = false,
-    update_cwd = true,
-    update_to_buf_dir = {
-      enable = true,
-      auto_open = true,
-    },
-    diagnostics = {
-      enable = false,
-      icons = {
-        hint = "",
-        info = "",
-        warning = "",
-        error = "",
-      },
-    },
-    update_focused_file = {
-      enable = true,
-      update_cwd = true,
-      ignore_list = {},
-    },
-    git = {
-      enable = true,
-      ignore = true,
-      timeout = 500,
-    },
-    view = {
-      width = 40,
-      height = 30,
-      hide_root_folder = false,
-      side = "left",
-      auto_resize = true,
-      mappings = {
-        custom_only = false,
-        list = {
-          { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-          { key = "h", cb = tree_cb "close_node" },
-          { key = "v", cb = tree_cb "vsplit" },
-        },
-      },
-      number = false,
-      relativenumber = false,
-    },
-    renderer = {
-      icons = {
-        webdev_colors = true,
-        glyphs = {
-          default = "",
-          symlink = "",
-          git = {
-            unstaged = "",
-            staged = "S",
-            unmerged = "",
-            renamed = "➜",
-            deleted = "",
-            untracked = "U",
-            ignored = "◌",
-          },
-          folder = {
-            default = "",
-            open = "",
-            empty = "",
-            empty_open = "",
-            symlink = "",
-          },
-        },
-      }
-    },
-    -- quit_on_open = 0,
-    -- git_hl = 1,
-    -- disable_window_picker = 0,
-    -- root_folder_modifier = ":t",
-    -- show_icons = {
-    --   git = 1,
-    --   folders = 1,
-    --   files = 1,
-    --   folder_arrows = 1,
-    --   tree_width = 30,
-    -- },
-  }
+  -- nvim_tree.setup {
+  --   disable_netrw = true,
+  --   hijack_netrw = true,
+  --   open_on_setup = false,
+  --   ignore_ft_on_setup = {
+  --     "startify",
+  --     "dashboard",
+  --     "alpha",
+  --   },
+  --   -- auto_close = true,
+  --   open_on_tab = false,
+  --   hijack_cursor = false,
+  --   update_cwd = true,
+  --   update_to_buf_dir = {
+  --     enable = true,
+  --     auto_open = true,
+  --   },
+  --   diagnostics = {
+  --     enable = false,
+  --     icons = {
+  --       hint = "",
+  --       info = "",
+  --       warning = "",
+  --       error = "",
+  --     },
+  --   },
+  --   update_focused_file = {
+  --     enable = true,
+  --     update_cwd = true,
+  --     ignore_list = {},
+  --   },
+  --   git = {
+  --     enable = true,
+  --     ignore = true,
+  --     timeout = 500,
+  --   },
+  --   view = {
+  --     width = 40,
+  --     height = 30,
+  --     hide_root_folder = false,
+  --     side = "left",
+  --     auto_resize = true,
+  --     mappings = {
+  --       custom_only = false,
+  --       list = {
+  --         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+  --         { key = "h", cb = tree_cb "close_node" },
+  --         { key = "v", cb = tree_cb "vsplit" },
+  --       },
+  --     },
+  --     number = false,
+  --     relativenumber = false,
+  --   },
+  --   renderer = {
+  --     icons = {
+  --       webdev_colors = true,
+  --       glyphs = {
+  --         default = "",
+  --         symlink = "",
+  --         git = {
+  --           unstaged = "",
+  --           staged = "S",
+  --           unmerged = "",
+  --           renamed = "➜",
+  --           deleted = "",
+  --           untracked = "U",
+  --           ignored = "◌",
+  --         },
+  --         folder = {
+  --           default = "",
+  --           open = "",
+  --           empty = "",
+  --           empty_open = "",
+  --           symlink = "",
+  --         },
+  --       },
+  --     }
+  --   },
+  --   -- quit_on_open = 0,
+  --   -- git_hl = 1,
+  --   -- disable_window_picker = 0,
+  --   -- root_folder_modifier = ":t",
+  --   -- show_icons = {
+  --   --   git = 1,
+  --   --   folders = 1,
+  --   --   files = 1,
+  --   --   folder_arrows = 1,
+  --   --   tree_width = 30,
+  --   -- },
+  -- }
+
+nvim_tree.setup()
 
 ------------------------------------------------------
 -- MAPPINGS
