85bb0a129280a923457765719e31f3cb7e79ede4
Madalin Popa <contact@madalinpopa.com>
2023-03-16 16:54:34 +0200

    Update neovim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -38,9 +38,6 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
   --
   -- In this case, we create a function that lets us more easily define mappings specific
   -- for LSP related items. It sets the mode, buffer and description for us each time.
@@ -93,7 +90,6 @@ local servers = {
   -- tsserver = {},
 
   emmet_ls = {
-    settings = {
       filetypes = {
         'djangohtml',
         'html',
@@ -103,8 +99,7 @@ local servers = {
         'sass',
         'scss',
         'less'
-      },
-    }
+    },
   },
 
   lua_ls = {
