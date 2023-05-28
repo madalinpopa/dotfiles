a6bba27e5192f3a4a5207a82c4d6f8dbd1993a41
Madalin Popa <contact@madalinpopa.com>
2023-05-28 14:18:48 +0300

    Add brew bin path

--- config/zsh/config.d/exports.zsh
+++ config/zsh/config.d/exports.zsh
@@ -24,3 +24,6 @@ fi
 if [ -f "/home/linuxbrew/.linuxbrew/bin/brew" ]; then
   eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 fi
+if [ -f "/opt/homebrew/bin/brew" ]; then
+  eval $(/opt/homebrew/bin/brew shellenv)
+fi
