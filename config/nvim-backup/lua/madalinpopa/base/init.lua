342b8ae409ea16f609786343131398c80ed2e9da
Madalin Popa <contact@madalinpopa.com>
2023-09-23 21:42:42 +0300

    Remove neovim backup

--- config/nvim-backup/lua/madalinpopa/base/init.lua
+++
@@ -1,8 +0,0 @@
-------------------------------------------------------
--- Author: Madalin Popa
--- Email: contact@madalinpopa.com
-------------------------------------------------------
-require("madalinpopa.base.options")
-require("madalinpopa.base.keymaps")
-require("madalinpopa.base.colorscheme")
-require("madalinpopa.base.filetypes")
--- config/nvim-backup/lua/madalinpopa/lsp/setup/emmet.lua
+++
@@ -1,8 +0,0 @@
-------------------------------------------------------
--- Author: Madalin Popa
--- Email: contact@madalinpopa.com
-------------------------------------------------------
-return {
-    filetypes = {'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'django-html',
-                 'htmldjango'}
-}
