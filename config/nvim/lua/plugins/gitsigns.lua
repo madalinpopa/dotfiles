b0c939c2cc79088f8acffd913adad4dc2e3384c7
Madalin Popa <contact@madalinpopa.com>
2023-10-15 18:35:59 +0300

    Update neovim config

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -4,7 +4,6 @@
 return {
     'lewis6991/gitsigns.nvim',
     opts = {
-        -- See `:help gitsigns.txt`
         signs = {
             add = { text = '+' },
             change = { text = '~' },
@@ -12,5 +11,31 @@ return {
             topdelete = { text = '‾' },
             changedelete = { text = '~' },
         },
+        on_attach = function(bufnr)
+            vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk,
+                { buffer = bufnr, desc = 'Preview git hunk' })
+
+            -- don't override the built-in and fugitive keymaps
+            local gs = package.loaded.gitsigns
+            vim.keymap.set({ 'n', 'v' }, ']c', function()
+                if vim.wo.diff then
+                    return ']c'
+                end
+                vim.schedule(function()
+                    gs.next_hunk()
+                end)
+                return '<Ignore>'
+            end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
+            vim.keymap.set({ 'n', 'v' }, '[c', function()
+                if vim.wo.diff then
+                    return '[c'
+                end
+                vim.schedule(function()
+                    gs.prev_hunk()
+                end)
+                return '<Ignore>'
+            end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
+        end,
     },
+
 }
--- config/old-nvim/lua/coderustle/lazyvim.lua
+++ config/old-nvim/lua/coderustle/lazyvim.lua
@@ -29,12 +29,8 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
-  -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
 
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
+
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
@@ -59,73 +55,6 @@ require('lazy').setup({
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
   },
 
-  -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
-
-  { -- Adds git releated signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
-
-  {
-    'rebelot/kanagawa.nvim',
-    config = function()
-      vim.cmd.colorscheme 'kanagawa'
-    end,
-  },
-
-  { -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
-
-
-
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
-
-  -- Fuzzy Finder (files, lsp, etc)
-  { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
-
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-  -- Only load if `make` is available. Make sure you have the system
-  -- requirements installed.
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    -- NOTE: If you are having trouble with this installation,
-    --       refer to the README for telescope-fzf-native for more instructions.
-    build = 'make',
-    cond = function()
-      return vim.fn.executable 'make' == 1
-    end,
-  },
-
-  { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    config = function()
-      pcall(require('nvim-treesitter.install').update { with_sync = true })
-    end,
-  },
 
 
 
