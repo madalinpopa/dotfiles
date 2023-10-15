f5b780928d2443af36f18c34294f786c04745e02
Madalin Popa <contact@madalinpopa.com>
2023-10-15 21:38:09 +0300

    Update pyright config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -144,7 +144,13 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-    pyright = {},
+    pyright = {
+        python = {
+            analysis = {
+                typeCheckingMode = "off",
+            }
+        }
+    },
     html = { filetypes = { 'html', 'twig', 'hbs', 'djangohtml' } },
     lua_ls = {
         Lua = {
