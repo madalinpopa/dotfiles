efa24ddcc97410e67bbf135e6795fa7dd8909182
Madalin Popa <contact@madalinpopa.com>
2023-10-16 13:44:24 +0000

    Update scripts

--- scripts/install-ohmyzsh.sh
+++ scripts/install-ohmyzsh.sh
@@ -11,8 +11,4 @@
 # Version: 0.1.0
 # Email: github@madalinpopa.com
 
-apt-get install zsh
-
-# chsh -s $(which zsh)
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
