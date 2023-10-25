7af413d22a2dfb74b4c3626070e5d11059c12e79
Madalin Popa <contact@madalinpopa.com>
2023-10-25 12:44:25 +0300

    Update install script

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 This is a repository with all my dotfiles.
 
-### Steps to install
-1. Create a python virtual environment `python3 -m venv .venv`
-2. Enable python environment `source .venv/bin/activate`
-3. Install requirements.txt `pip install -r requirements`
-4. Run `main.py`
+```
+curl 
+```
--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 set -x
 set -e
 
-
-echo "Start program.."
+# Repository URL for your dotfiles
+REPO_URL="https://github.com/madalinpopa/dotfiles.git"
+DOTFILES_DIR="${HOME}/.dotfiles"
 
 # Ensure Python and pip are installed
 command -v python3 >/dev/null 2>&1 || { echo "Python 3 is required but not installed."; exit 1; }
@@ -14,8 +15,19 @@ command -v pip3 >/dev/null 2>&1 || { echo "pip3 is required but not installed.";
 echo "Installing Dotbot..."
 pip3 install dotbot
 
+# Clone dotfiles repository (or pull if it's already cloned)
+if [ ! -d "${DOTFILES_DIR}" ]; then
+    echo "Cloning dotfiles repository..."
+    git clone "${REPO_URL}" "${DOTFILES_DIR}"
+else
+    echo "${DOTFILES_DIR} already exists. Pulling latest changes..."
+    cd "${DOTFILES_DIR}"
+    git pull origin master
+fi
+
 # Execute Dotbot
 echo "Running Dotbot..."
+cd "${DOTFILES_DIR}"
 dotbot -c conf.yaml
 
 echo "Dotfiles setup complete!"
