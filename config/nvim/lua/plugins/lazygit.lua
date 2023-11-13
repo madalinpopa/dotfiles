24e8992c8d977816c2cabc42b59675af53c96084
Madalin Popa <contact@madalinpopa.com>
2023-11-13 14:45:57 +0200

    Update lazygit neovim plugin

--- config/nvim/lua/plugins/lazygit.lua
+++ config/nvim/lua/plugins/lazygit.lua
@@ -1,9 +1,8 @@
-require("lazy").setup({
-    {
-        "kdheepak/lazygit.nvim",
-        -- optional for floating window border decoration
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-    },
-})
+return {
+   "kdheepak/lazygit.nvim",
+   -- optional for floating window border decoration
+   dependencies = {
+       "nvim-lua/plenary.nvim",
+   },
+}
+
