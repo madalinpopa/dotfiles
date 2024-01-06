57ebc292e4284ec4825da8b0e33cf975fed61b6a
Madalin Popa <contact@madalinpopa.com>
2024-01-06 16:34:12 +0200

    Update zshrc prompt

--- config/zsh/config.d/exports.zsh
+++ config/zsh/config.d/exports.zsh
@@ -9,12 +9,17 @@ export PATH=$HOME/.local/sbin:$PATH
 # http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_DATA_DIRS="/usr/local/share:/usr/share"
 
 # Set date as DD-MM-YYYY
 export DATE=$(date +%d-%m-%Y)
 
 # You may need to manually set your language environment
 export LANG=en_US.UTF-8
+
+# Enable  256 color support
+export TERM="xterm-256color"
 
 # Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
