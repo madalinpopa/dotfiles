b7fee0a9bfdbcc0bf6e7f6c4facb0680e11b17d6
Madalin Popa <contact@madalinpopa.com>
2024-01-06 16:37:54 +0200

    Update zsh prompt

--- config/zsh/config.d/prompt.zsh
+++ config/zsh/config.d/prompt.zsh
@@ -6,20 +6,9 @@ autoload -Uz vcs_info
 add-zsh-hook precmd vcs_info
 zstyle ':vcs_info:*' enable hg git cvs svn
 zstyle ':vcs_info:*' formats '%s|%b|%a|%i|%R|%r|%S|%m'
-zstyle ":completion:*" auto-description "specify: %d"
-zstyle ":completion:*" completer _complete _correct _approximate
-zstyle ":completion:*" format "Completing %d"
-zstyle ":completion:*" group-name ""
-zstyle ":completion:*" menu select=2
-zstyle ":completion:*:default" list-colors ${(s.:.)LS_COLORS}
-zstyle ":completion:*" list-colors ""
-zstyle ":completion:*" list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
-zstyle ":completion:*" matcher-list "" "m:{a-z}={A-Z}" "m:{a-zA-Z}={A-Za-z}" "r:|[._-]=* r:|=* l:|=*"
 zstyle ":completion:*" menu select=long
-zstyle ":completion:*" select-prompt %SScrolling active: current selection at %p%s
-zstyle ":completion:*" verbose true
-zstyle ":completion:*:*:kill:*:processes" list-colors "=(#b) #([0-9]#)*=0=01;31"
-zstyle ":completion:*:kill:*" command "ps -u $USER -o pid,%cpu,tty,cputime,cmd"
+zstyle ":completion:*" menu select=2
+
 
 # my prompt theme
 function promptSetup () {
