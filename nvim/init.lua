231072eb4ee417d81a59549c5abde27558ad38f0
Madalin Popa <contact@madalinpopa.com>
2024-04-02 10:20:14 +0300

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -247,3 +247,32 @@ cmp.setup {
         { name = 'luasnip' },
     },
 }
+
+-- Toggle terminal setup
+require("toggleterm").setup {
+        size = 20,
+        open_mapping = [[<c-\>]],
+        hide_numbers = true,
+        shade_filetypes = {},
+        shade_terminals = true,
+        shading_factor = 2,
+        start_in_insert = true,
+        insert_mappings = true,
+        persist_size = true,
+        direction = "float",
+        close_on_exit = true,
+        shell = vim.o.shell,
+        float_opts = {
+                border = "curved",
+                winblend = 0,
+                highlights = {
+                        border = "Normal",
+                        background = "Normal",
+                },
+        },
+}
+
+-- Telekasten setup
+require('telekasten').setup({
+        home = vim.fn.expand("~/notes"), -- Put the name of your notes directory here
+})
