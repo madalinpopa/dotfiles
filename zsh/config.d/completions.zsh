030b514f3c63e8583da1364f3a9b22725f645893
Madalin Popa <contact@madalinpopa.com>
2024-09-13 17:14:09 +0300

    Update completions.zsh

--- zsh/config.d/completions.zsh
+++ zsh/config.d/completions.zsh
@@ -1,16 +1,26 @@
+# -----------------------------------
+# Author: Madalin Popa              -
+# Email: coderustle@madalinpopa.com -
+# -----------------------------------
+
+# Ensure Zsh completion system is loaded
+autoload -Uz compinit
+compinit
 
 # Bun completions
-[ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
+BUN_COMPLETION="$HOME/.bun/_bun"
+[[ -s "$BUN_COMPLETION" ]] && source "$BUN_COMPLETION"
 
-# Nvm completions
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+# Nvm bash completions
+NVM_BASH_COMPLETION="$NVM_DIR/bash_completion"
+[[ -s "$NVM_BASH_COMPLETION" ]] && source "$NVM_BASH_COMPLETION"
 
 # Kubectl completions
-if [ -x "$(command -v kubectl)" ]; then
-  source <(kubectl completion zsh)
+if command -v kubectl &> /dev/null; then
+   source <(kubectl completion zsh)
 fi
 
-# # Kustomize completions
-if [ -x "$(command -v kustomize)" ]; then
+# Kustomize completions
+if command -v kustomize &> /dev/null; then
   source <(kustomize completion zsh)
 fi
