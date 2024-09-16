63cee0a6a269b9e761e3e1b9857ca25ecc0861b3
Madalin Popa <contact@madalinpopa.com>
2024-09-16 11:06:26 +0300

    Update remap.lua

--- nvim/lua/coderustle/remap.lua
+++ nvim/lua/coderustle/remap.lua
@@ -36,3 +36,19 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Add keympa for copilot to accept only one word
 vim.keymap.set('i', '<C-L>', '<Plug>(copilot-accept-word)')
+
+-- Keymap to create a new tab
+vim.keymap.set("n", "<leader>t", ":tabnew<CR>")
+
+-- Keymap to move between tabs
+vim.keymap.set("n", "<leader>1", "1gt")
+vim.keymap.set("n", "<leader>2", "2gt")
+vim.keymap.set("n", "<leader>3", "3gt")
+vim.keymap.set("n", "<leader>4", "4gt")
+vim.keymap.set("n", "<leader>5", "5gt")
+vim.keymap.set("n", "<leader>6", "6gt")
+vim.keymap.set("n", "<leader>7", "7gt")
+vim.keymap.set("n", "<leader>8", "8gt")
+vim.keymap.set("n", "<leader>9", "9gt")
+vim.keymap.set("n", "<leader>0", ":tablast<CR>")
+
