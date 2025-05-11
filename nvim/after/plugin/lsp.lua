8f977a8774b780cf53d7a2ed72a7a00f0c011669
Madalin Popa <contact@madalinpopa.com>
2025-05-11 12:46:48 +0300

    Fix lsp config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,6 +1,11 @@
+-- Setup Mason first
 require('mason').setup()
-require('mason-lspconfig').setup()
 
+-- Now, get the mason-lspconfig module
+local mason_lspconfig = require 'mason-lspconfig'
+
+-- Define your on_attach function, servers table, and capabilities
+-- (Your existing definitions for these are fine)
 local on_attach = function(_, bufnr)
     local nmap = function(keys, func, desc)
         if desc then
@@ -11,19 +16,14 @@ local on_attach = function(_, bufnr)
 
     nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
     nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
     nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
     nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
     nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
     nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
     nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
     nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-    -- See `:help K` for why this keymap
     nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
     nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-    -- Lesser used LSP functionality
     nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
     nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
     nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
@@ -31,8 +31,6 @@ local on_attach = function(_, bufnr)
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, '[W]orkspace [L]ist Folders')
 
-
-    -- Create a command `:Format` local to the LSP buffer
     vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
         vim.lsp.buf.format()
     end, { desc = 'Format current buffer with LSP' })
@@ -58,7 +56,6 @@ local servers = {
             templ = "html"
         }
     },
-
     emmet_ls = {
         filetypes = { "css", "templ", "tmpl", "html", "gohtml", "javascript" }
     },
@@ -71,30 +68,30 @@ local servers = {
     },
 }
 
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-
--- Ensure the servers above are installed
-
-local mason_lspconfig = require 'mason-lspconfig'
-
-
+-- Configure mason-lspconfig with a single setup call
 mason_lspconfig.setup {
-    ensure_installed = vim.tbl_keys(servers),
-
-}
-
-
-mason_lspconfig.setup_handlers {
-    function(server_name)
-        require('lspconfig')[server_name].setup {
-            capabilities = capabilities,
-
-            on_attach = on_attach,
-            settings = servers[server_name],
-            filetypes = (servers[server_name] or {}).filetypes,
-        }
-    end,
+    ensure_installed = vim.tbl_keys(servers), -- Your existing ensure_installed logic
+    handlers = {
+        -- This is the default handler function
+        function(server_name)
+            require('lspconfig')[server_name].setup {
+                capabilities = capabilities,
+                on_attach = on_attach,
+                settings = servers[server_name],
+                filetypes = (servers[server_name] or {}).filetypes,
+            }
+        end,
+        -- If you have custom handlers for specific servers, they would go here.
+        -- For example:
+        -- lua_ls = function()
+        --     require('lspconfig').lua_ls.setup {
+        --         capabilities = capabilities,
+        --         on_attach = on_attach,
+        --         settings = servers.lua_ls.Lua, -- or however you defined it
+        --     }
+        -- end,
+    }
 }
