c0b1daeef22b8c50c0a07c42f249364b001746d6
Madalin Popa <contact@madalinpopa.com>
2025-05-22 23:24:57 +0300

    Update conform and remove prettier plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,5 @@
 require("coderustle")
 
-vim.cmd [[
-  autocmd BufWritePre *.py lua vim.lsp.buf.format()
-  autocmd BufWritePre *.go lua vim.lsp.buf.format()
-  autocmd BufWritePre *.lua lua vim.lsp.buf.format()
-]]
-
 -- Highlight selection on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
